7723b6361ee30a34216f1fda51226271a7081dc2
wdyjason <ddd123321456555@qq.com>
2020-09-11 16:44:11 +0800

    [wdyjason] feat: finish practice

--- src/practice1.js
+++ src/practice1.js
@@ -1,2 +1,13 @@
-const parseData
+const parseData = (input) => {
+    const {data, column} = input
+    const result = data.map(rowData=> {
+        let returnRowData = {}
+        rowData.forEach((e, index) => {
+            returnRowData[column[index].name] = rowData[index]
+        })
+        return returnRowData
+    })
+    return result
+}
+
 export { parseData };
--- src/practice2.js
+++ src/practice2.js
@@ -1,2 +1,18 @@
-const inject
+const inject = (items, sections) => {
+    let offset = 0
+    sections.forEach(element => {
+        let injectIndex = element.index + offset
+        items.splice(injectIndex, 0, element.content)
+        offset = calcOffset(offset, element.index)    
+    })
+    return items
+}
+
+const calcOffset = (old, nowIndex) => {
+    if (old > nowIndex)
+        return old
+    else 
+        return old + 1
+}
+
 export { inject };
